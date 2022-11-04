73ffb7ec5ed792cca72f2e73d4cb3af2eb88a9ad
LouBRODA <115875561+LouBRODA@users.noreply.github.com>
2022-11-04 08:48:49 +0100

    Create detail.php
    add detail.php sans données

---
+++ Code/html/detail.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/detail.css">
+    <link rel="stylesheet" href="../css/header_footer.css">
+    <title>FromageOpédie</title>
+</head>
+<body>
+
+    <!-- Début de l'entête de page -->
+
+    <header>
+
+        <nav>
+
+            <div class="titre">
+                <a href="accueil.html"><h1>FromageOpédie</h1></a>
+            </div>
+
+            <div class="onglets">
+                <a href="fromage.php"><p>Fromages</p></a>
+                <a href="favoris.php"><p>Favoris</p></a>
+                <a href="histoire.html"><p>Histoire</p></a>
+                <a href="carte.php"><p>Carte</p></a>
+            </div>
+
+            <div class="log">
+                <a href="connexion.php">Connexion</a>
+                <a href="inscription.php">Inscription</a>
+            </div>
+
+        </nav>
+
+    </header>
+
+    <!-- Début du corp de code -->
+
+    <section>
+
+        <div class="entete">
+
+            <div class="image">
+                <img src="../Img/Image_Accueil.jpg" alt="Image Fromage">
+            </div>
+
+            <div class="titre">
+                <h1>St Nectaire</h1>
+            </div>
+
+            <div class="moyenne">
+                <h2>5/5</h2>
+            </div>
+
+        </div>
+
+        <div class="corps">
+
+            <div class="informations">    
+                <div class="departement">
+                    <h2>Département :</h2>
+                    <h3>Puy-de-Dôme</h3>
+                </div>
+                
+                <div class="lait">
+                    <h2>Type de lait :</h2>
+                    <h3>Vache</h3>
+                </div>
+                
+                <div class="vin">
+                    <h2>Vin associé :</h2>
+                    <h3>Rouge sec</h3>
+                </div>
+            </div>
+
+            <div class="avis">
+                <div class="note">
+                    <h2>Noter ce fromage !</h2>
+                </div>
+
+                <div class="commentaire">
+                    <h2>Liste des commentaires :</h2>
+                </div>
+            </div>
+
+            <div class="wiki">
+                <h2>En savoir plus : https://wikipedia.info.com</h2>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- Début du pied de page -->
+
+    <footer>
+
+        <div class="global_footer">
+
+            <div class="foot1">
+                <h3>Contact</h1>
+                <p>exemple@exemple.fr</p>
+            </div>
+    
+            <div class="foot2">
+                <h3>A propos</h1>
+                <p>Iut Clermont-Ferrand</p>
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+</html>
